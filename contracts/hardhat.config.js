339705bd265e72b4a81463b9386492b0d62633f8
Efemax00 <davidoders788@gmail.com>
2026-02-10 03:23:08 +0100

    Deploy BitcoinTestFaucet contract to local network

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const MIDL_RPC_URL = process.env.MIDL_RPC_URL || "https://testnet.rpc.midl.xyz";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0x0000000000000000000000000000000000000000000000000000000000000000";
+
 module.exports = {
   solidity: "0.8.24",
+  networks: {
+    midl: {
+      url: MIDL_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 4,
+    },
+  },
 };
